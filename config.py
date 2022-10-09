d1f2f05baa19978f5acac8651ba39d008c025950
paradox <76739784+sumpreme@users.noreply.github.com>
2022-10-09 14:35:13 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ host = "31.31.196.38"
 user = "u1721556_admin"
 password = "Lapik2022."
 bd_name = "u1721556_telegrambot"
-bot = telebot.TeleBot('5471405708:AAEw83oAsPHAxbEnIxEyG5oWwjjDf4rjqSs')
+bot = telebot.TeleBot('5626641804:AAF_SpZqEP249cBoy1w8vpIJHZRXi9NOqgs')
